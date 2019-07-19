621b24def2e842ece9934b3b9aefb31cd4a2413a
rugamaga <rugamaga@gmail.com>
2019-07-20 04:32:20 +0900

    implement first compare_and_swap()

--- src/first.rs
+++ src/first.rs
@@ -15,5 +15,10 @@ fn sub_sort(x: &mut [u32], up: bool) {
 }
 
 fn compare_and_swap(x: &mut [u32], up: bool) {
-    unimplemented!();
+    let mid_point = x.len() / 2;
+    for i in  0..mid_point {
+        if (x[i] > x[mid_point + i]) == up {
+            x.swap(i, mid_point + i);
+        }
+    }
 }
