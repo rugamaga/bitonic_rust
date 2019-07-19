55fa57c7e7e674922546dc8bd945e8cd4ea0fe76
rugamaga <rugamaga@gmail.com>
2019-07-20 04:29:19 +0900

    implement first sort()

--- src/first.rs
+++ src/first.rs
@@ -1,5 +1,9 @@
 pub fn sort(x: &mut [u32], up: bool) {
-    unimplemented!();
+    if x.len() <= 1 { return }
+    let mid_point = x.len() / 2;
+    sort(&mut x[..mid_point], true);
+    sort(&mut x[mid_point..], false);
+    sub_sort(x, up);
 }
 
 fn sub_sort(x: &mut [u32], up: bool) {
